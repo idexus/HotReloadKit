407c3542b7a086dce2047550863b7a1661f0f632
Pawel Krzywdzinski <pawel@dexus.pl>
2024-02-13 17:55:53 +0100

    build update

--- src/HotReloadKit.VSCode/buildvsix.sh
+++ src/HotReloadKit.VSCode/buildvsix.sh
@@ -2,6 +2,8 @@ rm service.zip
 cd ..
 rm -r ./HotReloadKit.VSCodeService/bin
 rm -r ./HotReloadKit.VSCodeService/obj
+rm -r ./HotReloadKit.VSCode/out
+rm -r ./HotReloadKit.VSCode/service
 zip -r service.zip HotReloadKit.VSCodeService Shared
 mv service.zip ./HotReloadKit.VSCode
 cd HotReloadKit.VSCode
